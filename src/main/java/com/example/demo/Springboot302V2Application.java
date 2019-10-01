6d1273e7bd23150de3c3bd135b52e9a280d13cf0
KathyOlsen <katherinekolsen@verizon.net>
2019-10-01 11:13:48 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Springboot302V2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot302V2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot302V2Application.class, args);
+    }
+
+}
